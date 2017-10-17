4178d0b617488f67d77daeb0b06f71ff1cfdde23
Deba <aldealdo@gmail.com>
2017-10-17 16:57:53 +0200

    Debut systeme longues

--- app/get_actions.php
+++ app/get_actions.php
@@ -2,18 +2,34 @@
 function catchGet(){
 	$request = array(
 		'page' => 'index',
-		'filter' => 'all'
+		'filter' => 'all',
+		'lang' => 'fr'
 	);
 	if (!empty($_GET)){
 
 		if (isset($_GET['querying'])&&!empty($_GET['querying'])) {
-			$request['page'] = explode('.',  $_GET['querying'])[0];
+
+			if ( sizeOf(explode('/', $_GET['querying'])) > 2 ) {
+				$request['page'] = '404';
+			} else {
+				if (sizeOf(explode('/', $_GET['querying'])) == 2) {
+					$request['lang'] = explode('/',  $_GET['querying'])[0];
+					$file = explode('/',  $_GET['querying'])[1];
+					$request['page'] = explode('.',  $file)[0];
+				} else {
+					$request['lang'] = 'fr';
+
+					$request['page'] = explode('.',  $_GET['querying'])[0];
+				}
+			}
+
 		}
 
 		if (isset($_GET['filter'])) {
 			$request['filter'] = explode('.',  $_GET['filter'])[0];
 		} 
 	}
+
 	return $request;
 }
 
